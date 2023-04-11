dc69d26d22f8ce8249c638d241ce1ba96027f1eb
vicarb <vcardenasb@protonmail.com>
2023-04-11 13:21:19 -0400

    tap exit

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
+import Image from 'next/image';
+import { FaTimes } from 'react-icons/fa';
 
-const artworks = [
+
+
+
+type Artwork = {
+  name: string;
+  image: string;
+  description?: string;
+};
+const artworks: Artwork[] = [
   { name: 'The Creation of Adam', image: '/creation.jpg' },
   { name: 'Starry Night', image: '/starry_night.jpg' },
   { name: 'Mona Lisa', image: '/mona_lisa.jpg' },
@@ -11,44 +21,36 @@ const artworks = [
   { name: 'Girl with a Pearl Earring', image: '/girl_with_pearl_earring.jpg' }
 ];
 
-type Artwork = {
-  name: string;
-  image: string;
-  description?: string;
-};
 
-export const Landing = () => {
-  const [selectedArtwork, setSelectedArtwork] = useState<Artwork | null>(null);
+const Landing = () => {
+  const [showImage, setShowImage] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
 
-  const handleArtworkClick = (artwork: Artwork) => {
-    setSelectedArtwork(artwork);
+  const handleImageClick = (image: string) => {
+    setSelectedImage(image);
+    setShowImage(true);
   };
 
-  const handleCloseModal = () => {
-    setSelectedArtwork(null);
+  const handleImageClose = () => {
+    setShowImage(false);
+    setSelectedImage('');
   };
 
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold text-sky mb-8">Welcome to our Art Collection</h1>
       <div className="w-full md:w-3/4 lg:w-1/2">
-        <img
-          src={artworks[0].image}
-          alt={artworks[0].name}
-          className="w-full h-auto mb-8 cursor-pointer"
-          onClick={() => handleArtworkClick(artworks[0])}
-        />
+        <button onClick={() => handleImageClick(artworks[0].image)}>
+          <Image src="https://cdn.shopify.com/s/files/1/0336/2701/3259/articles/Michelangelo_the_creation_of_adam_meaning_f356496b-ef77-4759-bc81-f23e864cf2b4.jpg?v=1663084517" alt={artworks[0].name} width={1200} height={800} layout="responsive" className="w-full h-auto mb-8" />
+        </button>
       </div>
       <div className="flex flex-wrap justify-center">
         {artworks.slice(1, 9).map((artwork: Artwork) => (
           <div key={artwork.name} className="m-4 w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
             <div className="bg-white rounded-md shadow-md">
-              <img
-                src="https://cdn.shopify.com/s/files/1/0336/2701/3259/articles/Michelangelo_the_creation_of_adam_meaning_f356496b-ef77-4759-bc81-f23e864cf2b4.jpg?v=1663084517"
-                alt={artwork.name}
-                className="w-full h-auto rounded-t-md cursor-pointer"
-                onClick={() => handleArtworkClick(artwork)}
-              />
+              <button onClick={() => handleImageClick("https://cdn.shopify.com/s/files/1/0336/2701/3259/articles/Michelangelo_the_creation_of_adam_meaning_f356496b-ef77-4759-bc81-f23e864cf2b4.jpg?v=1663084517")}>
+                <Image src="https://cdn.shopify.com/s/files/1/0336/2701/3259/articles/Michelangelo_the_creation_of_adam_meaning_f356496b-ef77-4759-bc81-f23e864cf2b4.jpg?v=1663084517" alt={artwork.name} width={500} height={500} layout="responsive" className="w-full h-auto rounded-t-md" />
+              </button>
               <div className="p-4">
                 <h2 className="text-lg font-bold text-gray-800 mb-2">{artwork.name}</h2>
                 <p className="text-gray-600">{artwork.description}</p>
@@ -57,16 +59,16 @@ export const Landing = () => {
           </div>
         ))}
       </div>
-      {selectedArtwork && (
-        <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex justify-center items-center">
-          <div className="bg-white rounded-md shadow-lg p-4">
-            <button className="absolute top-4 right-4 text-gray-600" onClick={handleCloseModal}>
-              X
-            </button>
-            <img src="https://cdn.shopify.com/s/files/1/0336/2701/3259/articles/Michelangelo_the_creation_of_adam_meaning_f356496b-ef77-4759-bc81-f23e864cf2b4.jpg?v=1663084517" alt={selectedArtwork.name} className="w-full h-auto" />
-          </div>
+      {showImage && (
+        <div className="fixed top-0 left-0 h-screen w-screen flex justify-center items-center bg-black bg-opacity-80" onClick={handleImageClose}>
+          <button onClick={handleImageClose} className="absolute top-5 right-5 text-white">
+            <FaTimes size={32} />
+          </button>
+          <Image src={selectedImage} alt="Selected Artwork" layout="responsive" width={1200} height={800} />
         </div>
       )}
     </div>
   );
 };
+
+export default Landing;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { Landing } from '@/components/Landing/Landing'
+
+import Landing from '@/components/Landing/Landing'
 
 const inter = Inter({ subsets: ['latin'] })
 
