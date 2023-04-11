df3aad2482861ee3f0f42009a334ef98423287f5
vicarb <vcardenasb@protonmail.com>
2023-04-11 13:03:07 -0400

    image tap

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -1,42 +1,72 @@
-import React from 'react'
-import Image from 'next/image';
+import React, { useState } from 'react';
 
 const artworks = [
-  { name: 'The Creation of Adam', image: '/public/creation.jpg' },
-  { name: 'Starry Night', image: 'path/to/starry_night.jpg' },
-  { name: 'Mona Lisa', image: 'path/to/mona_lisa.jpg' },
-  { name: 'The Persistence of Memory', image: 'path/to/persistence_of_memory.jpg' },
-  { name: 'The Scream', image: 'path/to/the_scream.jpg' },
-  { name: 'The Great Wave off Kanagawa', image: 'path/to/great_wave.jpg' },
-  { name: 'The Last Supper', image: 'path/to/last_supper.jpg' },
-  { name: 'Girl with a Pearl Earring', image: 'path/to/girl_with_pearl_earring.jpg' }
+  { name: 'The Creation of Adam', image: '/creation.jpg' },
+  { name: 'Starry Night', image: '/starry_night.jpg' },
+  { name: 'Mona Lisa', image: '/mona_lisa.jpg' },
+  { name: 'The Persistence of Memory', image: '/persistence_of_memory.jpg' },
+  { name: 'The Scream', image: '/the_scream.jpg' },
+  { name: 'The Great Wave off Kanagawa', image: '/great_wave.jpg' },
+  { name: 'The Last Supper', image: '/last_supper.jpg' },
+  { name: 'Girl with a Pearl Earring', image: '/girl_with_pearl_earring.jpg' }
 ];
 
 type Artwork = {
-    name: string;
-    image: string;
-    description?: string;
+  name: string;
+  image: string;
+  description?: string;
+};
+
+export const Landing = () => {
+  const [selectedArtwork, setSelectedArtwork] = useState<Artwork | null>(null);
+
+  const handleArtworkClick = (artwork: Artwork) => {
+    setSelectedArtwork(artwork);
   };
-  export const Landing = () => {
-    return (
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-sky mb-8">Welcome to our Art Collection</h1>
-        <div className="w-full md:w-3/4 lg:w-1/2">
-          <Image src="https://cdn.shopify.com/s/files/1/0336/2701/3259/articles/Michelangelo_the_creation_of_adam_meaning_f356496b-ef77-4759-bc81-f23e864cf2b4.jpg?v=1663084517" alt={artworks[0].name} width={1200} height={800} layout="responsive" className="w-full h-auto mb-8" />
-        </div>
-        <div className="flex flex-wrap justify-center">
-          {artworks.slice(1, 9).map((artwork: Artwork) => (
-            <div key={artwork.name} className="m-4 w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
-              <div className="bg-white rounded-md shadow-md">
-                <Image src="https://cdn.shopify.com/s/files/1/0336/2701/3259/articles/Michelangelo_the_creation_of_adam_meaning_f356496b-ef77-4759-bc81-f23e864cf2b4.jpg?v=1663084517" alt={artwork.name} width={500} height={500} layout="responsive" className="w-full h-auto rounded-t-md" />
-                <div className="p-4">
-                  <h2 className="text-lg font-bold text-gray-800 mb-2">{artwork.name}</h2>
-                  <p className="text-gray-600">{artwork.description}</p>
-                </div>
+
+  const handleCloseModal = () => {
+    setSelectedArtwork(null);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-sky mb-8">Welcome to our Art Collection</h1>
+      <div className="w-full md:w-3/4 lg:w-1/2">
+        <img
+          src={artworks[0].image}
+          alt={artworks[0].name}
+          className="w-full h-auto mb-8 cursor-pointer"
+          onClick={() => handleArtworkClick(artworks[0])}
+        />
+      </div>
+      <div className="flex flex-wrap justify-center">
+        {artworks.slice(1, 9).map((artwork: Artwork) => (
+          <div key={artwork.name} className="m-4 w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+            <div className="bg-white rounded-md shadow-md">
+              <img
+                src={artwork.image}
+                alt={artwork.name}
+                className="w-full h-auto rounded-t-md cursor-pointer"
+                onClick={() => handleArtworkClick(artwork)}
+              />
+              <div className="p-4">
+                <h2 className="text-lg font-bold text-gray-800 mb-2">{artwork.name}</h2>
+                <p className="text-gray-600">{artwork.description}</p>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    );
-  };
+      {selectedArtwork && (
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex justify-center items-center">
+          <div className="bg-white rounded-md shadow-lg p-4">
+            <button className="absolute top-4 right-4 text-gray-600" onClick={handleCloseModal}>
+              X
+            </button>
+            <img src={selectedArtwork.image} alt={selectedArtwork.name} className="w-full h-auto" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
