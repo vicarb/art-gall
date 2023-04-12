885762bd806b80f030302ce74bc96ffeef7dbf3c
vicarb <vcardenasb@protonmail.com>
2023-04-11 20:23:14 -0400

    img name

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -24,16 +24,17 @@ const artworks: Artwork[] = [
 
 const Landing = () => {
   const [showImage, setShowImage] = useState(false);
-  const [selectedImage, setSelectedImage] = useState('');
+  const [selectedArtwork, setSelectedArtwork] = useState<Artwork | null>(null);
 
-  const handleImageClick = (image: string) => {
-    setSelectedImage(image);
+  const handleImageClick = (artwork: Artwork) => {
+    setSelectedArtwork(artwork);
     setShowImage(true);
   };
+  
 
   const handleImageClose = () => {
     setShowImage(false);
-    setSelectedImage('');
+    setSelectedArtwork(null);
   };
 
 
@@ -41,15 +42,16 @@ const Landing = () => {
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold text-sky mb-8">Welcome to our Art Collection</h1>
       <div className="w-full md:w-3/4 lg:w-1/2">
-        <button onClick={() => handleImageClick(artworks[0].image)}>
+        <button onClick={() => handleImageClick(artworks[0])}>
+          <Image src={artworks[0].image} alt={artworks[0].name} width={500} height={500} layout="responsive" className="w-full h-auto rounded-t-md" />
         </button>
       </div>
       <div className="flex flex-wrap justify-center">
         {artworks.slice(1, 9).map((artwork: Artwork) => (
           <div key={artwork.name} className="m-4 w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
             <div className="bg-white rounded-md shadow-md">
-              <button onClick={() => handleImageClick("https://cdn.shopify.com/s/files/1/0336/2701/3259/articles/Michelangelo_the_creation_of_adam_meaning_f356496b-ef77-4759-bc81-f23e864cf2b4.jpg?v=1663084517")}>
-                <Image src="https://cdn.shopify.com/s/files/1/0336/2701/3259/articles/Michelangelo_the_creation_of_adam_meaning_f356496b-ef77-4759-bc81-f23e864cf2b4.jpg?v=1663084517" alt={artwork.name} width={500} height={500} layout="responsive" className="w-full h-auto rounded-t-md" />
+              <button onClick={() => handleImageClick(artwork)}>
+                <Image src="https://thumbs.dreamstime.com/b/sculpture-pieta-lamentation-christ-michelangelo-buonarroti-pushkin-museum-moscow-russia-july-191390304.jpg" alt={artwork.name} width={500} height={500} layout="responsive" className="w-full h-auto rounded-t-md" />
               </button>
               <div className="p-4">
                 <h2 className="text-lg font-bold text-gray-800 mb-2">{artwork.name}</h2>
@@ -64,7 +66,13 @@ const Landing = () => {
           <button onClick={handleImageClose} className="absolute top-5 right-5 text-white">
             <FaTimes size={32} />
           </button>
-          <Image src={selectedImage} alt="Selected Artwork" layout="intrinsic" width={1200} height={800} />
+          <div className="flex flex-col items-center">
+            <Image src="https://thumbs.dreamstime.com/b/sculpture-pieta-lamentation-christ-michelangelo-buonarroti-pushkin-museum-moscow-russia-july-191390304.jpg" alt="Selected Artwork" layout="intrinsic" width={1200} height={800} />
+            <div className="bg-white w-full p-4">
+              <h2 className="text-2xl font-bold text-gray-800 mb-2">{selectedArtwork.name}</h2>
+              <p className="text-gray-600">{selectedArtwork.description}</p>
+            </div>
+          </div>
         </div>
       )}
     </div>
